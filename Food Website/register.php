f3ab616be318ba52c244349686ea698e0d03a21e
Tron_ke <122445766+chegefranklynn@users.noreply.github.com>
2023-07-27 20:17:21 +0300

    Add files via upload

--- Food Website/register.php
+++ Food Website/register.php
@@ -20,5 +20,44 @@
         <br>
         <input type="submit" value="Register">
     </form>
+    <?php
+// Database connection and configuration
+$servername = "your_database_servername";
+$username = "your_database_username";
+$password = "your_database_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve user input
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Hash the password for security
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prepare and execute the SQL query to insert the user into the database
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $hashed_password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful! You can now <a href='login.php'>login</a>.";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
+
 </body>
 </html>
